bc93e37ca853ef426a25effd7cf7ff4164d69156
KKashi-Hatake <shashankgamerid@gmail.com>
2026-03-15 18:52:57 +0530

    Final Commit

---
+++ public/js/main.js
@@ -0,0 +1,216 @@
+const createUserButton = document.getElementById('create-user');
+const username = document.getElementById('username');
+const localVideo = document.getElementById('localVideo');
+const remoteVideo = document.getElementById('remoteVideo');
+const micBtn = document.getElementById("toggle-mic");
+const remoteBtn = document.getElementById("toggle-remote-audio");
+const endCallBtn = document.getElementById("end-call-btn")
+
+
+let localStream = null;
+let caller = []
+const socket = io();
+
+
+
+const PeerConnection = (function(){
+    let peerConnection = null;
+
+
+    
+    function createPeerConnection(){
+        peerConnection = new RTCPeerConnection({
+            iceServers: [
+                { urls: 'stun:stun.l.google.com:19302' }
+            ]
+        });
+
+
+        // add local stream to peer connection
+        localStream.getTracks().forEach(track => {
+            peerConnection.addTrack(track, localStream);
+        });
+
+
+        // listen for remote stream and add to peer connection
+        peerConnection.ontrack = (event) => {
+            remoteVideo.srcObject = event.streams[0];
+        };
+
+
+        // listen for ice candidate
+        peerConnection.onicecandidate = (event) => {
+            if (event.candidate) {
+                socket.emit('ice-candidate', event.candidate);
+            }
+        };
+
+
+        return peerConnection;
+    }
+
+    return {
+        getInstance:()=>{
+            if(!peerConnection){
+                peerConnection = createPeerConnection();
+            }
+            return peerConnection;
+        }
+    }
+})()
+
+
+// handle browser events
+createUserButton.addEventListener('click', () => {
+    const usernameValue = username.value;
+    if (usernameValue.trim() !== '') {
+        
+        socket.emit('join-user', usernameValue);
+        const usernameContainer = document.querySelector('.username-input');
+        usernameContainer.style.display = 'none';
+    }
+});
+
+
+endCallBtn.addEventListener("click",(e)=>{
+    socket.emit("call-ended", caller)
+})
+
+
+micBtn.addEventListener("click", () => {
+
+    const audioTrack = localStream.getAudioTracks()[0];
+
+    if (audioTrack.enabled) {
+        audioTrack.enabled = false;
+        micBtn.textContent = "Unmute Mic";
+    } else {
+        audioTrack.enabled = true;
+        micBtn.textContent = "Mute Mic";
+    }
+
+});
+
+
+
+remoteBtn.addEventListener("click", () => {
+
+    remoteVideo.muted = !remoteVideo.muted;
+
+    if (remoteVideo.muted) {
+        remoteBtn.textContent = "Unmute Remote";
+    } else {
+        remoteBtn.textContent = "Mute Remote";
+    }
+
+});
+
+
+
+
+
+
+
+// handle socket events
+socket.on('joined', (users) => {
+    console.log("Users joined", users);
+    const allusers = document.getElementById('allusers');
+    allusers.innerHTML = '';
+    Object.values(users).forEach(user => {
+        const li = document.createElement('li');
+        li.textContent =`${user.username} ${user.username===username.value? "(You)":"" } `;
+
+        if(user.username !== username.value){
+            const callButton = document.createElement('button');
+            callButton.classList.add('call-btn');
+            callButton.addEventListener('click', () => {
+                startCall(user.username);
+            });
+            const img = document.createElement('img');
+            img.setAttribute("src", "/images/phone.png");
+            img.setAttribute("width", "20");
+            callButton.appendChild(img);
+            li.appendChild(callButton);
+        }
+
+        allusers.appendChild(li);
+    });
+});
+
+
+
+socket.on("offer", async ({from, to, offer})=>{
+    const pc = PeerConnection.getInstance();
+    // set remote description
+    await pc.setRemoteDescription(offer)
+    const answer = await pc.createAnswer();
+    await pc.setLocalDescription(answer);
+    socket.emit('answer', {from, to, answer:pc.localDescription})
+    caller = [from, to]
+})
+
+
+socket.on("answer", async ({from, to, answer})=>{
+    const pc = PeerConnection.getInstance();
+    await pc.setRemoteDescription(answer);
+
+    endCallBtn.style.display = 'block'
+    socket.emit("end-call", {from, to});
+    caller = [from, to]
+})
+
+
+socket.on("ice-candidate", async (candidate)=>{
+    const pc = PeerConnection.getInstance();
+    await pc.addIceCandidate(new RTCIceCandidate(candidate))
+})
+
+
+socket.on("end-call",({form, to})=>{
+    endCallBtn.style.display='block'
+})
+
+
+socket.on("call-ended",()=>{
+    endCall();
+    endCallBtn.style.display='none'
+
+})
+
+
+
+
+// start call function
+async function startCall(user) {
+    if(username.value===""){
+        alert(`Please create your user first to call "${user}"`)
+    }
+    console.log("Starting call with", user);
+    const pc = PeerConnection.getInstance();
+    const offer = await pc.createOffer();
+    await pc.setLocalDescription(offer);
+    socket.emit("offer", {from:username.value, to:user, offer:pc.localDescription})
+}
+
+
+// initialize local stream
+async function startMyVideo(){
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+        localStream = stream;
+        localVideo.srcObject = stream;
+    } catch (error) {
+        
+    }
+}
+startMyVideo()
+
+async function endCall() {
+    const pc = PeerConnection.getInstance();
+    if(pc){
+        pc.close()
+    }
+}
+
+
+
---
+++ server.js
@@ -0,0 +1,61 @@
+import express from "express";
+import {createServer} from "http";
+import { Server } from "socket.io";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+const allUsers = {}
+
+// /your/system/path
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// exposing public directory to outside world
+app.use(express.static(join(__dirname + '/public')));
+
+// handle GET request to the root path
+app.get("/", (req, res) => {
+    return res.sendFile(join(__dirname +'/app/index.html'));
+});
+
+//handle socket connection
+io.on("connection", (socket) => {
+    console.log("A user connected", socket.id);
+    socket.on('join-user', (username) => {
+        console.log("A user joined", username);
+        allUsers[username] = {username, id: socket.id};
+
+        // inform everyone that someone has joined
+        io.emit('joined', allUsers);
+    });
+
+
+    socket.on("offer", ({from, to, offer})=>{
+        io.to(allUsers[to].id).emit("offer", {from, to, offer})
+    })
+
+    socket.on("answer", ({from, to, answer})=>{
+        io.to(allUsers[from].id).emit("answer", {from, to, answer})
+    })
+
+    socket.on("ice-candidate", candidate=>{
+        socket.broadcast.emit("ice-candidate", candidate)
+    })
+
+    socket.on("end-call", ({from, to})=>{
+        io.to(allUsers[to].id).emit("end-call", {from, to})
+    })
+
+    socket.on("call-ended", caller=>{
+        io.to(allUsers[caller[0]].id).emit("call-ended")
+        io.to(allUsers[caller[1]].id).emit("call-ended")
+    })
+});
+
+server.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
+
